6d0945cfd9adc0b06c6aa7a3a58a84f67c9318f9
Lucas Marçal Coutinho <coutinho0604@gmail.com>
2020-08-01 12:11:59 -0300

    Correction

--- scripts/settings.sh
+++ scripts/settings.sh
@@ -32,10 +32,10 @@ echo "${COLOR_GREEN}"
 echo "-+-+-+-+-+- Configurando Git -+-+-+-+-+-"
 echo "${COLOR_RESET}"
 
-git config --global user.name "Lucas Marçal Coutinho"
-git config --global user.email coutinho0604@gmail.com
-git config --global core.editor code
-git config --list
+# git config --global user.name "Lucas Marçal Coutinho"
+# git config --global user.email coutinho0604@gmail.com
+# git config --global core.editor code
+# git config --list
 
 
 # -+-+-+-+-+- Typora -+-+-+-+-+-
