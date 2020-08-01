8233cd141e3b13e16d5cb7babcb002e2adc07d49
Lucas Marçal Coutinho <coutinho0604@gmail.com>
2020-08-01 10:33:20 -0300

    Added PostgreSQL

--- scripts/apps-apt.sh
+++ scripts/apps-apt.sh
@@ -154,6 +154,18 @@ echo "${COLOR_RESET}"
 # sudo apt update && sudo apt install obs-studio -y
 
 
+# -+-+-+-+-+- PostgreSQL -+-+-+-+-+-
+
+echo "${COLOR_BLUE}"
+echo "-+-+-+-+-+- Instalando PostgreSQL -+-+-+-+-+-"
+echo "${COLOR_RESET}"
+
+# sudo sh -c 'echo "deb [arch=amd64] http://apt.postgresql.org/pub/repos/apt $(lsb_release -cs)-pgdg main" > /etc/apt/sources.list.d/pgdg.list'
+# wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | sudo apt-key add -
+# sudo apt update
+# sudo apt install postgresql postgresql-12 postgresql-client-12 postgresql-contrib libpq-dev postgresql-server-dev-12 pgadmin4 -y
+
+
 # -+-+-+-+-+- Python 3 -+-+-+-+-+-
 
 echo "${COLOR_BLUE}"
