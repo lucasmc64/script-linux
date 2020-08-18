08b9cc04da8134e6bb4c11fe1cf8bee4d30f9da9
Lucas Marçal Coutinho <coutinho0604@gmail.com>
2020-08-18 19:54:37 -0300

    Some logic fixes and version updates

--- scripts/others/jdk8.sh
+++ scripts/others/jdk8.sh
@@ -16,25 +16,25 @@ echo "${COLOR_BLUE}"
 echo "-+-+-+-+-+- Instalando JDK 8 -+-+-+-+-+-"
 echo "${COLOR_RESET}"
 
-wget -O jdk-8u241-linux-x64.tar.gz http://enos.itcollege.ee/~jpoial/allalaadimised/jdk8/jdk-8u241-linux-x64.tar.gz
+wget -O jdk-8u261-linux-x64.tar.gz http://enos.itcollege.ee/~jpoial/allalaadimised/jdk8/jdk-8u261-linux-x64.tar.gz
 sudo mkdir /usr/lib/jvm
-cd /usr/lib/jvm
-sudo tar -xvzf ~/Downloads/jdk-8u241-linux-x64.tar.gz
+sudo tar -xvzf ./jdk-8u261-linux-x64.tar.gz
+mv ./jdk1.8.0_261 /usr/lib/jvm
 sudo cp /etc/environment /etc/environment.bkp
 PATH_VARIABLE=`grep "PATH=\"*\"" /etc/environment`
 OLD_PATH_CONTENT=`echo ${PATH_VARIABLE:6:${#PATH_VARIABLE}-7}`
-ADD_TO_PATH=`echo ":/usr/lib/jvm/jdk1.8.0_241/bin:/usr/lib/jvm/jdk1.8.0_241/db/bin:/usr/lib/jvm/jdk1.8.0_241/jre/bin"`
+ADD_TO_PATH=`echo ":/usr/lib/jvm/jdk1.8.0_261/bin:/usr/lib/jvm/jdk1.8.0_261/db/bin:/usr/lib/jvm/jdk1.8.0_261/jre/bin"`
 NEW_PATH_CONTENT=`echo ${OLD_PATH_CONTENT}${ADD_TO_PATH}`
 OTHERS_VARIABLES=`grep -v "PATH=\"*\"" /etc/environment`
-JAVA_VARIABLES=`printf "J2SDKDIR=\"/usr/lib/jvm/jdk1.8.0_241\"\nJ2REDIR=\"/usr/lib/jvm/jdk1.8.0_241/jre\"\nJAVA_HOME=\"/usr/lib/jvm/jdk1.8.0_241\"\nDERBY_HOME=\"/usr/lib/jvm/jdk1.8.0_241/db\"\n"`
+JAVA_VARIABLES=`printf "J2SDKDIR=\"/usr/lib/jvm/jdk1.8.0_261\"\nJ2REDIR=\"/usr/lib/jvm/jdk1.8.0_261/jre\"\nJAVA_HOME=\"/usr/lib/jvm/jdk1.8.0_261\"\nDERBY_HOME=\"/usr/lib/jvm/jdk1.8.0_261/db\"\n"`
 sudo printf "PATH=\"${NEW_PATH_CONTENT}\"\n\n${OTHERS_VARIABLES}\n\n${JAVA_VARIABLES}" > /etc/environment
-sudo update-alternatives --install "/usr/bin/java" "java" "/usr/lib/jvm/jdk1.8.0_241/bin/java" 0
-sudo update-alternatives --install "/usr/bin/javac" "javac" "/usr/lib/jvm/jdk1.8.0_241/bin/javac" 0
-sudo update-alternatives --set java /usr/lib/jvm/jdk1.8.0_241/bin/java
-sudo update-alternatives --set javac /usr/lib/jvm/jdk1.8.0_241/bin/javac
+sudo update-alternatives --install "/usr/bin/java" "java" "/usr/lib/jvm/jdk1.8.0_261/bin/java" 0
+sudo update-alternatives --install "/usr/bin/javac" "javac" "/usr/lib/jvm/jdk1.8.0_261/bin/javac" 0
+sudo update-alternatives --set java /usr/lib/jvm/jdk1.8.0_261/bin/java
+sudo update-alternatives --set javac /usr/lib/jvm/jdk1.8.0_261/bin/javac
 update-alternatives --list java
 update-alternatives --list javac
-sudo rm -rf ./jdk-8u241-linux-x64.tar.gz
+sudo rm -rf ./jdk-8u261-linux-x64.tar.gz
 java -version
 
 echo "${COLOR_RED}"
