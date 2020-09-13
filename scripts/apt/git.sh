4f83462ff777cf28bf9158422335441295a71e6a
Lucas Marçal Coutinho <coutinho0604@gmail.com>
2020-09-13 09:28:14 -0300

    Added the Git manual

--- scripts/apt/git.sh
+++ scripts/apt/git.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
-
-# -+-+-+-+-+- Variáveis -+-+-+-+-+-
+# => Variáveis
 
 COLOR_RED=`tput setaf 1`
 COLOR_GREEN=`tput setaf 2`
@@ -9,13 +8,12 @@ COLOR_YELLOW=`tput setaf 3`
 COLOR_BLUE=`tput setaf 4`
 COLOR_RESET=`tput sgr0`
 
-
-# -+-+-+-+-+- Git -+-+-+-+-+-
+# => Git
 
 echo "${COLOR_BLUE}"
-echo "-+-+-+-+-+- Instalando Git -+-+-+-+-+-"
+echo "=> Instalando Git"
 echo "${COLOR_RESET}"
 
 add-apt-repository ppa:git-core/ppa -y
 apt update
-sudo apt install git -y
+sudo apt install git git-man -y
