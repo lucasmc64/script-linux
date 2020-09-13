baf4ca83393055edeca3522692845c9f563e207b
Lucas Marçal Coutinho <coutinho0604@gmail.com>
2020-09-13 09:26:45 -0300

    Opera is now installed in APT in the normal version

---
+++ scripts/apt/opera.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# => Variáveis
+
+COLOR_RED=`tput setaf 1`
+COLOR_GREEN=`tput setaf 2`
+COLOR_YELLOW=`tput setaf 3`
+COLOR_BLUE=`tput setaf 4`
+COLOR_RESET=`tput sgr0`
+
+# => Opera
+
+echo "${COLOR_BLUE}"
+echo "=> Instalando Opera Developer"
+echo "${COLOR_RESET}"
+
+wget -O - http://deb.opera.com/archive.key | sudo apt-key add -
+sudo sh -c 'echo "deb http://deb.opera.com/opera-stable/ stable non-free" >> /etc/apt/sources.list.d/opera.list'
+sudo apt update
+sudo apt install opera-stable -y
