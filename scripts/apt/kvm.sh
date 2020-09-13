600c811b1405ee217d3074e90abdfa491ae3de54
Lucas Marçal Coutinho <coutinho0604@gmail.com>
2020-09-13 09:30:36 -0300

    User is added to KVM in another file now

--- scripts/apt/kvm.sh
+++ scripts/apt/kvm.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
-
-# -+-+-+-+-+- Variáveis -+-+-+-+-+-
+# => Variáveis
 
 COLOR_RED=`tput setaf 1`
 COLOR_GREEN=`tput setaf 2`
@@ -9,15 +8,13 @@ COLOR_YELLOW=`tput setaf 3`
 COLOR_BLUE=`tput setaf 4`
 COLOR_RESET=`tput sgr0`
 
-
-# -+-+-+-+-+- KVM -+-+-+-+-+-
+# => KVM
 
 echo "${COLOR_BLUE}"
-echo "-+-+-+-+-+- Instalando KVM -+-+-+-+-+-"
+echo "=> Instalando KVM"
 echo "${COLOR_RESET}"
 
 sudo apt install cpu-checker -y
 egrep -c '(vmx|svm)' /proc/cpuinfo
 kvm-ok
 sudo apt install qemu-kvm -y
-sudo adduser $USER kvm
