42ba119dd6ae1a5604b636fd3f65c46cc3e5d84a
Lucas Marçal Coutinho <coutinho0604@gmail.com>
2020-09-13 09:27:37 -0300

    Little changes

--- scripts/apt/dconf.sh
+++ scripts/apt/dconf.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
-
-# -+-+-+-+-+- Variáveis -+-+-+-+-+-
+# => Variáveis
 
 COLOR_RED=`tput setaf 1`
 COLOR_GREEN=`tput setaf 2`
@@ -9,11 +8,10 @@ COLOR_YELLOW=`tput setaf 3`
 COLOR_BLUE=`tput setaf 4`
 COLOR_RESET=`tput sgr0`
 
-
-# -+-+-+-+-+- dconf -+-+-+-+-+-
+# => dconf
 
 echo "${COLOR_BLUE}"
-echo "-+-+-+-+-+- Instalando dconf -+-+-+-+-+-"
+echo "=> Instalando dconf"
 echo "${COLOR_RESET}"
 
 sudo apt install dconf-cli -y
--- scripts/apt/flatpak.sh
+++ scripts/apt/flatpak.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
-
-# -+-+-+-+-+- Variáveis -+-+-+-+-+-
+# => Variáveis
 
 COLOR_RED=`tput setaf 1`
 COLOR_GREEN=`tput setaf 2`
@@ -9,11 +8,10 @@ COLOR_YELLOW=`tput setaf 3`
 COLOR_BLUE=`tput setaf 4`
 COLOR_RESET=`tput sgr0`
 
-
-# -+-+-+-+-+- Suporte a Flatpak -+-+-+-+-+-
+# => Suporte a Flatpak
 
 echo "${COLOR_BLUE}"
-echo "-+-+-+-+-+- Instalando Suporte à Flatpaks -+-+-+-+-+-"
+echo "=> Instalando Suporte à Flatpaks"
 echo "${COLOR_RESET}"
 
 sudo apt install flatpak -y
