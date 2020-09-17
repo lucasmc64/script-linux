9fa6aa14c14a0401dd74b862433c8994d488268c
Lucas Marçal Coutinho <coutinho0604@gmail.com>
2020-09-16 21:45:33 -0300

    Correction

--- scripts/apt/opera.sh
+++ scripts/apt/opera.sh
@@ -15,6 +15,6 @@ echo "=> Instalando Opera Developer"
 echo "${COLOR_RESET}"
 
 wget -O - http://deb.opera.com/archive.key | sudo apt-key add -
-sudo sh -c 'echo "deb http://deb.opera.com/opera-stable/ stable non-free" >> /etc/apt/sources.list.d/opera.list'
+sudo sh -c 'echo "deb http://deb.opera.com/opera-stable/ stable non-free" >> /etc/apt/sources.list.d/opera-stable.list'
 sudo apt update
 sudo apt install opera-stable -y
