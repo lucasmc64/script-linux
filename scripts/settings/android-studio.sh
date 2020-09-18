7acf362dbd1a2138a4ddcbd45892268e841d1ff6
Lucas Marçal Coutinho <coutinho0604@gmail.com>
2020-09-18 07:43:28 -0300

    Corrections

--- scripts/settings/android-studio.sh
+++ scripts/settings/android-studio.sh
@@ -14,11 +14,11 @@ echo "${COLOR_GREEN}"
 echo "=> Adicionando variáveis de Ambiente do Android Studio"
 echo "${COLOR_RESET}"
 
+CURRENT_USER="lucasmc64"
 PATH_VARIABLE=`grep "PATH=\"*\"" /etc/environment`
 OLD_PATH_CONTENT=`echo ${PATH_VARIABLE:6:${#PATH_VARIABLE}-7}`
-ADD_TO_PATH=`echo ":/home/${USER}/Android/Sdk/emulator:/home/${USER}/Android/Sdk/tools:/home/${USER}/Android/Sdk/tools/bin:/home/${USER}/Android/Sdk/platform-tools"`
+ADD_TO_PATH=`echo ":/home/${CURRENT_USER}/Android/Sdk/emulator:/home/${CURRENT_USER}/Android/Sdk/tools:/home/${CURRENT_USER}/Android/Sdk/tools/bin:/home/${CURRENT_USER}/Android/Sdk/platform-tools"`
 NEW_PATH_CONTENT=`echo ${OLD_PATH_CONTENT}${ADD_TO_PATH}`
 OTHERS_VARIABLES=`grep -v "PATH=\"*\"" /etc/environment`
-ANDROID_VARIABLES=`printf "ANDROID_HOME=\"/home/${USER}/Android/Sdk\"\n"`
-sudo apt update
+ANDROID_VARIABLES=`printf "ANDROID_HOME=\"/home/${CURRENT_USER}/Android/Sdk\"\n"`
 sudo printf "PATH=\"${NEW_PATH_CONTENT}\"\n\n${OTHERS_VARIABLES}\n\n${ANDROID_VARIABLES}" > /etc/environment
