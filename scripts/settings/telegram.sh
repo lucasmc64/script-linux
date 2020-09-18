24f5b87c0143e3363bbf71a520216a0f7e9953c3
Lucas Marçal Coutinho <coutinho0604@gmail.com>
2020-09-18 07:43:10 -0300

    Dracula theme for telegram

---
+++ scripts/settings/telegram.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# => Variáveis
+
+COLOR_RED=`tput setaf 1`
+COLOR_GREEN=`tput setaf 2`
+COLOR_YELLOW=`tput setaf 3`
+COLOR_BLUE=`tput setaf 4`
+COLOR_RESET=`tput sgr0`
+
+# => Telegram
+
+echo "${COLOR_GREEN}"
+echo "=> Baixando tema Dracula para Telegram"
+echo "${COLOR_RESET}"
+
+git clone https://github.com/dracula/telegram.git
