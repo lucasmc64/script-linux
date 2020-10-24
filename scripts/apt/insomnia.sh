fca1730c006bfa6c1f27609ccfaaee6788adf93e
Lucas Marçal Coutinho <coutinho0604@gmail.com>
2020-10-24 16:08:20 -0300

    Re-correction

--- scripts/apt/insomnia.sh
+++ scripts/apt/insomnia.sh
@@ -14,7 +14,7 @@ echo "${COLOR_BLUE}"
 echo "=> Instalando Insomnia Core"
 echo "${COLOR_RESET}"
 
-echo "deb https://dl.bintray.com/getinsomnia/Insomnia/" \
+echo "deb https://dl.bintray.com/getinsomnia/Insomnia /" \
     | sudo tee -a /etc/apt/sources.list.d/insomnia.list &&
 wget --quiet -O - https://insomnia.rest/keys/debian-public.key.asc \
     | sudo apt-key add - &&
