cccd9e05500137b6c0693199eff44e14d563a2ef
Lucas Marçal Coutinho <coutinho0604@gmail.com>
2020-11-01 10:10:37 -0300

    Little corrections

--- scripts/apt/update.sh
+++ scripts/apt/update.sh
@@ -14,11 +14,11 @@ echo "${COLOR_RED}"
 echo "=> Atualizando repositórios e pragramas instalados"
 echo "${COLOR_RESET}"
 
-sudo apt clean &&
+sudo apt clean
+sudo dpkg --configure -a
+sudo apt install -f
+sudo apt dist-upgrade --fix-missing -y
 sudo apt update -m &&
-sudo dpkg --configure -a &&
-sudo apt install -f &&
-sudo apt dist-upgrade --fix-missing -y &&
-sudo apt upgrade -y &&
-sudo apt autoclean &&
+sudo apt upgrade -y
+sudo apt autoclean
 sudo apt autoremove --purge -y
