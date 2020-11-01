c4c702da9a785e0a5858aa9fd9b87380d66e1383
Lucas Marçal Coutinho <coutinho0604@gmail.com>
2020-11-01 10:08:48 -0300

    Removed PPA

--- scripts/apt/postgresql.sh
+++ scripts/apt/postgresql.sh
@@ -14,7 +14,4 @@ echo "${COLOR_BLUE}"
 echo "=> Instalando PostgreSQL"
 echo "${COLOR_RESET}"
 
-sudo sh -c 'echo "deb [arch=amd64] http://apt.postgresql.org/pub/repos/apt $(lsb_release -cs)-pgdg main" > /etc/apt/sources.list.d/pgdg.list' &&
-wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | sudo apt-key add - &&
-sudo apt update &&
-sudo apt install postgresql postgresql-12 postgresql-client-12 postgresql-contrib libpq-dev postgresql-server-dev-12 pgadmin4 -y
+sudo apt install postgresql -y
